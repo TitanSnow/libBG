61d161efd1e45b69fb520f4bf3b07af02e1cd32f
TitanSnow <sweeto@live.cn>
2017-03-26 11:38:56 +0800

    do modify as #1
    use global option instead of housekeeping

--- xmake.lua
+++ xmake.lua
@@ -1,24 +1,21 @@
 set_project("libBG")
 
-function housekeeping()  -- default options
-    set_languages("c99","cxx98")
-    set_warnings("all")
-    add_includedirs("$(buildir)")
+-- default global options for each targets
+set_languages("c99","cxx98")
+set_warnings("all")
+add_includedirs("$(buildir)")
 
-    if is_mode("release") then
-        add_cxflags("-march=native")
-        set_optimize("fastest")
+if is_mode("release") then
+    add_cxflags("-march=native")
+    set_optimize("fastest")
 
-    elseif is_mode("debug") then
-        set_optimize("fast")
-        set_symbols("debug")
-    end
+elseif is_mode("debug") then
+    set_optimize("fast")
+    set_symbols("debug")
 end
 
 target("bg")  -- main target
-    housekeeping()
     set_kind("$(kind)")
-
     add_files("src/bigint.cpp")
     add_headers("src/bigint.h")
 
@@ -30,7 +27,6 @@ target_end()
 function maketest(op)  -- test targets
     target("testprog" .. op)
         add_deps("bg")
-        housekeeping()
         set_kind("binary")
 
         add_files("test/b" .. op .. ".cpp")
