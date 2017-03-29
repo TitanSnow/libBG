73eb45eb29d049d6aeb6659535bc1d4ff1ee8db0
TitanSnow <sweeto@live.cn>
2017-03-29 08:07:27 +0800

    use getrusage instead of timediff

--- test/test.py
+++ test/test.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 import subprocess
-from time import time
 class Runner:
 	def __init__(self, args):
 		self.__process = subprocess.Popen(args, universal_newlines=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 	def run(self, input):
-		self.__start_time = time()
 		self.__output=self.__process.communicate(input)[0]
-		self.__end_time = time()
 
 	def get_output(self):
 		return self.__output
-
-	def get_running_time(self):
-		return self.__end_time - self.__start_time
 
 from random import randint
 times = 10000
@@ -34,7 +28,6 @@ def mymod(a, b):
 	if bool(a>=0) == bool(b>=0):
 		return a % b
 	return -(abs(a) % abs(b)) if a<0 else abs(a) % abs(b)
-total_time = 0
 for i in range(times):
 	a = randint(lo,hi)
 	b = randint(lo,hi)
@@ -58,8 +51,9 @@ for i in range(times):
 	run = Runner("./testprog"+op)
 	run.run(str(a) + " " + str(b))
 	opt = run.get_output()
-	total_time += run.get_running_time()
 	if opt!=str(rst):
 		if debugMode:print(opt)
 		exit(1)
+import resource as res
+total_time=res.getrusage(res.RUSAGE_CHILDREN).ru_utime
 print(op+' totaltime= '+str(total_time)+'s')
